c41e5cd25efbfd11f439afb3c523e25e81b93c30
N1k-3l <t9092121909@gmail.com>
2021-04-19 02:35:52 +0300

    15_puzzle game complete!

--- 15.py
+++ 15.py
@@ -70,15 +70,63 @@ def w_move(a):
     pri(mtrx)
 ##
 
+# for shuffle
 
-#i = randint(0, 3)
-#j = randint(0, 3)
 
-#mtrx = [
-#     ['01', '02', '03', '04'],
-#     ['05', '06', '07', '08'],
-#     ['09', '10', '11', 'XX'],
-#     ['13', '14', '15', '12']]
+def a_move_s(a):
+    MOVE = mtrx[i][j]
+    try:
+        XX = mtrx[i][j - 1]
+        mtrx[i][j - 1] = MOVE
+        mtrx[i][j] = XX
+    except IndexError:
+        pass
+    return a
+
+
+def d_move_s(a):
+    MOVE = mtrx[i][j]
+    try:
+        XX = mtrx[i][j + 1]
+        mtrx[i][j + 1] = MOVE
+        mtrx[i][j] = XX
+    except IndexError:
+        pass
+    return a
+
+
+def s_move_s(a):
+    MOVE = mtrx[i][j]
+    try:
+        XX = mtrx[i + 1][j]
+        mtrx[i + 1][j] = MOVE
+        mtrx[i][j] = XX
+    except IndexError:
+        pass
+
+
+def w_move_s(a):
+    MOVE = mtrx[i][j]
+    try:
+        XX = mtrx[i - 1][j]
+        mtrx[i - 1][j] = MOVE
+        mtrx[i][j] = XX
+    except IndexError:
+        pass
+
+###
+
+difficult = str.lower(input('Choose your destiny! (Fingerprint easy or hard): \n'))
+if difficult == 'easy':
+    diff = 5
+    print('Easy game start now!')
+else:
+    diff = 25
+    print('Hard game start now!')
+    
+moves = []
+for i in range(diff):
+    moves.append(randint(1, 4))
 
 win_mtrx = [
     ['01', '02', '03', '04'],
@@ -87,12 +135,31 @@ win_mtrx = [
     ['13', '14', '15', 'XX']]
 
 mtrx = win_mtrx
-for string_ in mtrx:
-    random.shuffle(string_)
 
-#temp = mtrx[i][j]
-#mtrx[i][j] = 'XX'
-#mtrx[3][3] = temp
+i = (coord('XX', mtrx))[0]
+j = ((coord('XX', mtrx))[1])
+
+# >> Shuffling
+for move in moves:
+    if move == 1:
+        if i != 0:
+            w_move_s(mtrx)
+            i = (coord('XX', mtrx))[0]
+            j = ((coord('XX', mtrx))[1])
+    elif move == 2:
+        d_move_s(mtrx)
+        i = (coord('XX', mtrx))[0]
+        j = ((coord('XX', mtrx))[1])
+    elif move == 3:
+        s_move_s(mtrx)
+        i = (coord('XX', mtrx))[0]
+        j = ((coord('XX', mtrx))[1])
+    elif move == 4:
+        if j != 0:
+            a_move_s(mtrx)
+            i = (coord('XX', mtrx))[0]
+            j = ((coord('XX', mtrx))[1])
+
 
 win_mtrx = [
     ['01', '02', '03', '04'],
@@ -101,11 +168,11 @@ win_mtrx = [
     ['13', '14', '15', 'XX']]
 pri(mtrx)
 
-i = (coord('XX', mtrx))[0]
-j = ((coord('XX', mtrx))[1])
-pri(win_mtrx)
+turns = 0
+#
 while mtrx != win_mtrx:
     move = input('Enter your move!  (use w, a, s, d)\n:')
+    turns += 1
     if move == 'w':
         if i != 0:
             w_move(mtrx)
@@ -132,4 +199,6 @@ while mtrx != win_mtrx:
             pri(mtrx)
     else:
         print('Use w, a, s, d keys only!')
+        turns -= 1
 print('You win \n TA DA !!! \n \\ (•◡•) /')
+print('You make ', turns, ' moves')
