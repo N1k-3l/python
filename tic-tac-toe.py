224d6f1b9dba22cb3a14b48fb99cc081df7eb8a0
N1k-3l <t9092121909@gmail.com>
2021-04-24 19:17:16 +0300

    Basic logic added. TODO: WIN_CONDITIONS

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,2 +1,37 @@
-#
+import random
 
+FIELD = list(range(1,10))
+X_SIDE = 'X'
+O_SIDE = '0'
+def print_field(field):
+    print(''' \n\n
+---------''')
+    for i in range(0, 9, 3):
+        print((FIELD[i:i+3]))
+        print('---------')
+    print('\n\n')
+
+def game_end():
+    return True
+
+def turn(field):
+    try:
+        key = int(input('Input 1-9 to put on X:'))
+        if key in field:
+            field[key-1] = X_SIDE
+            return field
+        raise ValueError
+    except ValueError:
+       print('Error move!')
+
+def comp_turn(field):
+    valid_moves = [index for index, element in enumerate(field) if element != X_SIDE]
+    print(valid_moves)
+    field[(random.choice(valid_moves))] = O_SIDE
+
+def main():
+    while game_end():
+        print_field(FIELD)
+        turn(FIELD)
+        comp_turn(FIELD)
+main()
