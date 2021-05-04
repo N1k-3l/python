b00c910bb26bc7425f79ee399d05d400c5ace05a
N1k-3l <t9092121909@gmail.com>
2021-05-04 10:29:05 +0300

    few changes in  ClassWork 5-2

--- my_class_2_inheritage.py
+++ my_class_2_inheritage.py
@@ -6,7 +6,7 @@ def vvod():
         abc = list(input('введите стороны фигуры через пробел: ').split())
         if abc != []:
             if check(abc):
-                abc_int = list(map(int, abc))
+                abc_int = tuple(map(int, abc))
                 return abc_int
         print('вводите только числа!')
 
@@ -29,17 +29,19 @@ def main():
         vvod_storon = vvod()
         if len(vvod_storon) in RULES:
             while True:
-                if len(vvod_storon) == 3 and not isnt_real_check(vvod_storon):
+                if len(vvod_storon) == 3 and isnt_real_check(vvod_storon) is not True:
                     print('такой треугольник построить нельзя')
                     main()
                 FIGURE = RULES[len(vvod_storon)](vvod_storon)
+                FIGURE.sides
                 FIGURE.output()
-                input('press any key to exit')
+                input('\n press any key to exit \n')
                 sys.exit()
         print('введите от 1 до 4 сторон!')
 
 class Tetragon(object):
     name = 'Tetragon'
+
     def __init__(self, arg):
         self.sides = arg
         self.arg = arg
@@ -60,6 +62,7 @@ class Tetragon(object):
 
 class Rectangle(Tetragon):
     name = 'Rectangle'
+
     def __init__(self, arg):
         self.sides = arg * 2
         self.arg = arg
@@ -71,6 +74,7 @@ class Rectangle(Tetragon):
 
 class Quadrant(Rectangle):
     name = 'Quadrant'
+
     def __init__(self, arg):
         self.sides = arg * 4
         self.arg = arg
@@ -79,32 +83,12 @@ class Quadrant(Rectangle):
 
 class Triangle(Tetragon):
     name = 'Triangle'
-    def __init__(self, arg):
-        self.sides = arg
-    #     ''''''
-    #     if not self.isnt_real_check():
-    #         raise ValueError('This isn\'t Triangle')
-
-    # def isnt_real_check(self):
-    #     """a + b > c, b + c > a, a + c > b."""
-    #     if ((self.sides[0] < (self.sides[1] + self.sides[2]))
-    #     and (self.sides[1] < (self.sides[2] + self.sides[0]))
-    #     and (self.sides[2] < (self.sides[0] + self.sides[1]))):
-    #         return True
 
     def square(self):
         p = self.perimeter() / 2
         S = sqrt(p * (p - self.sides[0]) * (p - self.sides[1]) * (p - self.sides[2]))
         return S
 
-RULES = {1:Quadrant, 2:Rectangle, 3:Triangle, 4:Tetragon}
-
-
-T = [2, 2, 2]
-
-T2 = [3, 4, 5]
-
-t = Triangle(T)
-t2 = Triangle(T2)
+RULES = {1: Quadrant, 2: Rectangle, 3: Triangle, 4: Tetragon}
 
 main()
